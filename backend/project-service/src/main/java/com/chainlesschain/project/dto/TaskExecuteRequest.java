66659b9c9abeb20ae4bd926d78a8f62abcce025c
longfa <896075341@qq.com>
2025-12-21 09:34:53 +0800

    fix: 更新validation注解从javax到jakarta
    Spring Boot 3.x使用Jakarta EE，需要将验证注解从:
    - javax.validation.constraints.NotBlank
    改为:
    - jakarta.validation.constraints.NotBlank
    
    修复文件:
    - TaskExecuteRequest.java
    - ProjectCreateRequest.java

--- backend/project-service/src/main/java/com/chainlesschain/project/dto/TaskExecuteRequest.java
+++ backend/project-service/src/main/java/com/chainlesschain/project/dto/TaskExecuteRequest.java
@@ -1,7 +1,7 @@
 package com.chainlesschain.project.dto;
 
 import lombok.Data;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Map;
 
